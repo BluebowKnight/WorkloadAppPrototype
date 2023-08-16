e3693aaece7bfe565971205db051898d5d9536d9
Bluebow_Knight <bulebow_knight@outlook.com>
2023-08-16 21:37:31 +0100

    Parsing json files and display in log.

--- app/src/main/java/com/example/mwltrackapp/MainActivity.java
+++ app/src/main/java/com/example/mwltrackapp/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.mwltrackapp;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.mwltrackapp.Data.DataInfor;
+import com.example.mwltrackapp.Data.DataParser;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<DataInfor> ratingEntries = DataParser.parseJsonFromFile(this, R.raw.test_data);
+
+        for (DataInfor entry : ratingEntries) {
+            String month = entry.getMonth();
+            String day = entry.getDay();
+            String year = entry.getYear();
+            String time = entry.getTime();
+            String mwlrating = entry.getMWLRating();
+            String sleepRating = entry.getSleepRating();
+
+            // 现在你可以使用解析后的数据进行操作，例如显示在 TextView 中
+            String data = "Date: " + month + " " + day + ", " + year + "\n"
+                    + "Time: " + time + "\n"
+                    + "MWL Rating: " + mwlrating + "\n"
+                    + "Sleep Rating: " + sleepRating + "\n\n";
+
+            // 将数据添加到你的 TextView 或其他视图中
+            Log.d(TAG, "Data:" + data);
+        }
     }
 }
